edab479e3ffdf8440142ef69f0608f1855001622
Max Denyshchuk <97800661+mamamamamaa@users.noreply.github.com>
2023-06-24 18:10:06 +0300

    Update useMatchGame.ts

--- src/hooks/useMatchGame.ts
+++ src/hooks/useMatchGame.ts
@@ -19,15 +19,6 @@ export const useMatchGame = (
     let matches: number;
 
     if (matchesRemaining === 0) return;
-    //
-    // if (matchesRemaining === matchesPerMove + 1) {
-    //   matches = 1;
-    // } else if (matchesRemaining <= matchesPerMove) {
-    //   matches = matchesRemaining;
-    // } else {
-    //   const remainder = matchesRemaining % (matchesPerMove + 1);
-    //   matches = remainder === 0 ? 1 : matchesPerMove - remainder + 1;
-    // }
 
     if (matchesRemaining === matchesPerMove + 1) {
       matches = 1;
