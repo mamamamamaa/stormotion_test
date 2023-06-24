7385b416eba2598438d75f48e1d74b1c86d29325
mamamamamaa <maks.denishchuk@gmail.com>
2023-06-24 13:45:41 +0300

    fix up

--- src/utils/isValidScore.ts
+++ src/utils/isValidScore.ts
@@ -1,9 +1,13 @@
 import { Score } from "../types/matchGame.ts";
 
 export const isValidScore = (score: any): score is Score => {
+  const date = new Date(score.date);
+
+  // @ts-ignore
+  const isRightDate = date !== "Invalid Date";
+
   const isRightTotalMatches = typeof score.totalMatches === "number";
   const isRightMatchesPerMove = typeof score.matchesPerMove === "number";
-  const isRightDate = new Date(score.date) !== "Invalid Date";
   const isRightWinner =
     typeof score.winner === "string" &&
     (score.winner === "USER" || score.winner === "AI");
